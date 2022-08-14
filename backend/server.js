4f27ab13a0fbc004ee9b02060d40bde7aa84f257
vhrvoje2 <vhrvoje2@yahoo.com>
2022-08-14 16:40:58 +0200

    Add backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db")
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Support Desk API" });
+});
+
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
